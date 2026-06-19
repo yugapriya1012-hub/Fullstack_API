13868bc520f17ff862ad82b31487efc78b40abe0
yugavijay <yugapriya1012@gmail.com>
2026-06-19 17:50:33 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ if os.path.exists("images"):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["https://yugapriya1012-hub.github.io", "http://localhost", "http://127.0.0.1:5500"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
